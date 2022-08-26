af653e20dd9f326c1cb4af9a5add6918d6fe7cfb
BrianS-TICS <briansanchezaguilar@gmail.com>
2022-08-26 14:42:07 -0600

    add Problema 14 - 15

---
+++ Problema-15.js
@@ -0,0 +1,14 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+
+const reverseSeq = n => {
+    let array = [];
+    for (let i = n; i > 0; i--) {
+
+        array = [...array, i];
+    }
+
+    return array;
+};
