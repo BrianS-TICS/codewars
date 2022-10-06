16e0a343256e0a1bf2ba2d3bc5b6c3c03c17960f
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2022-10-06 08:38:14 -0600

    add renive spaces and Unique in orden

---
+++ UniqueInOrder.js
@@ -0,0 +1,18 @@
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder = function (iterable) {
+    const elementos = [...iterable];
+    const cadenaRetorno = [];
+    for (let i = 0; i < elementos.length; i++) {
+        if (elementos[i] !== elementos[i + 1]) {
+            cadenaRetorno.push(elementos[i])
+        }
+    }   
+    return cadenaRetorno;
+}
